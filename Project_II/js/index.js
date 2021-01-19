b90003e504841527000dc21afb332c23ddd0bc64
Ryan <ryanlucas2018gz@gmail.com>
2021-01-19 02:45:41 -0300

    Atualizando...

--- Project_II/js/index.js
+++ Project_II/js/index.js
@@ -1,1 +1,14 @@
-var n = 10;
+const Triangle = function(counter,limit){
+    const string = "#";
+    if(isNaN(counter) || isNaN(limit)){
+        console.log("Coloque somente números!")
+    }else{
+        while(counter < limit){
+            counter = counter+1;
+            let input = string.length*counter;
+            console.log(string.repeat(input));
+        }
+    }
+}
+
+Triangle(10,20);
