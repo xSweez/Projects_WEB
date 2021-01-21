e3e25d297f667cbbd12d4e1dc664368085289289
Ryan <ryanlucas2018gz@gmail.com>
2021-01-21 02:49:39 -0300

    Triangle Test .JS

---
+++ Project_II/js/triangle.js
@@ -0,0 +1,15 @@
+const Triangle = function(counter,limit){
+    const string = "#";
+    if(isNaN(counter) || isNaN(limit)){
+        console.log("Coloque somente números!")
+    }else{
+        while(counter < limit){
+            counter = counter+1;
+            let input = string.length*counter;
+            console.log(string.repeat(input));
+        }
+    }
+}
+
+Triangle(0,7);
+
